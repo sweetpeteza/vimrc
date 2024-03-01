3cb70a31b0a5bfd40467cc5113be4571ff8d1617
sweetpeteza <peter@playbk.io>
2024-03-01 10:02:16 +0200

    feat: installed silicon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -50,6 +50,34 @@ return {
             require("litee.gh").setup()
         end,
     },
+    {
+        "michaelrommel/nvim-silicon",
+        lazy = true,
+        cmd = "Silicon",
+        config = function()
+            require("silicon").setup({
+                -- Configuration here, or leave empty to use defaults
+                font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+                theme = "Dracula",
+                background = "#94e2d5",
+                to_clipboard = true,
+                output = function()
+                    return "/home/peter/Pictures/Silicon" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
+                end,
+                command = "silicon",
+                -- a string or function returning a string that defines the title showing in the image
+                -- only works in silicon versions greater than v0.5.1
+                window_title = function()
+                    return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+                end,
+                language = function()
+                    return vim.bo.filetype
+                end,
+                no_line_number = true,
+
+            })
+        end
+    },
     'aquach/vim-http-client',
     {
         'akinsho/bufferline.nvim',
