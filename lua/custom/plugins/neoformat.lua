53932405bba0c1891838e0d800b8923d5d5134e1
sweetpeteza <peter@playbk.io>
2025-02-01 12:20:27 +0200

    feat: limited neoformat for certain files

--- lua/custom/plugins/neoformat.lua
+++ lua/custom/plugins/neoformat.lua
@@ -7,5 +7,8 @@ return {
 
     vim.g.neoformat_enabled_sql = { "sleek" }
     --vim.g.neoformat_enabled_python = { 'black', 'docformatter' }
+    vim.g.neoformat_enabled_cs = { "csharpier" }
+    vim.g.neoformat_enabled_typescript = { "prettier" }
+    vim.g.neoformat_enabled_typescriptreact = { "prettier" }
   end
 }
